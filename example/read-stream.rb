76fc90397fb1f5c3c563d1a52b63ee9fa95a87d3
Kouhei Sutou <kou@clear-code.com>
2017-04-06 22:56:39 +0900

    Add more patterns to examples

--- example/read-stream.rb
+++ example/read-stream.rb
@@ -4,9 +4,16 @@ require "arrow"
 
 Arrow::IO::MemoryMappedFile.open("/tmp/logs-stream.arrow", :read) do |input|
   Arrow::IPC::StreamReader.open(input) do |reader|
-    reader.each do |record_batch|
-      record_batch.each do |record|
-        p record[:count]
+    fields = reader.schema.fields
+    reader.each_with_index do |record_batch, i|
+      puts("=" * 40)
+      puts("record-batch[#{i}]:")
+      fields.each do |field|
+        field_name = field.name
+        values = record_batch.collect do |record|
+          record[field_name]
+        end
+        puts("#{field_name}: #{values.inspect}")
       end
     end
   end
--- example/write-stream.rb
+++ example/write-stream.rb
@@ -3,19 +3,44 @@
 require "arrow"
 
 fields = [
-  Arrow::Field.new("count", :uint32),
+  Arrow::Field.new("uint8",  :uint8),
+  Arrow::Field.new("uint16", :uint16),
+  Arrow::Field.new("uint32", :uint32),
+  Arrow::Field.new("uint64", :uint64),
+  Arrow::Field.new("int8",   :int8),
+  Arrow::Field.new("int16",  :int16),
+  Arrow::Field.new("int32",  :int32),
+  Arrow::Field.new("int64",  :int64),
+  Arrow::Field.new("float",  :float),
+  Arrow::Field.new("double", :double),
 ]
 schema = Arrow::Schema.new(fields)
 
 Arrow::IO::FileOutputStream.open("/tmp/logs-stream.arrow", false) do |output|
   Arrow::IPC::StreamWriter.open(output, schema) do |writer|
-    counts = [1, 2, 4, 8]
-    arrow_counts = Arrow::UInt32Array.new(counts)
+    uints = [1, 2, 4, 8]
+    ints = [1, -2, 4, -8]
+    floats = [1.1, -2.2, 4.4, -8.8]
+    columns = [
+      Arrow::UInt8Array.new(uints),
+      Arrow::UInt16Array.new(uints),
+      Arrow::UInt32Array.new(uints),
+      Arrow::UInt64Array.new(uints),
+      Arrow::Int8Array.new(ints),
+      Arrow::Int16Array.new(ints),
+      Arrow::Int32Array.new(ints),
+      Arrow::Int64Array.new(ints),
+      Arrow::FloatArray.new(floats),
+      Arrow::DoubleArray.new(floats),
+    ]
 
-    record_batch = Arrow::RecordBatch.new(schema, 4, [arrow_counts])
+    record_batch = Arrow::RecordBatch.new(schema, 4, columns)
     writer.write_record_batch(record_batch)
 
-    record_batch = Arrow::RecordBatch.new(schema, 3, [arrow_counts.slice(1, 3)])
+    sliced_columns = columns.collect do |column|
+      column.slice(1, 3)
+    end
+    record_batch = Arrow::RecordBatch.new(schema, 3, sliced_columns)
     writer.write_record_batch(record_batch)
   end
 end
