02ef1b63e65a67a69cb7f8f07c64c5023dac525a
Kouhei Sutou <kou@clear-code.com>
2017-04-30 23:13:39 +0900

    Follow namespace change

--- example/read-batch.rb
+++ example/read-batch.rb
@@ -1,9 +1,23 @@
 #!/usr/bin/env ruby
+#
+# Copyright 2017 Kouhei Sutou <kou@clear-code.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 require "arrow"
 
-Arrow::IOMemoryMappedFile.open("/tmp/batch.arrow", :read) do |input|
-  Arrow::IPCFileReader.open(input) do |reader|
+Arrow::MemoryMappedFile.open("/tmp/batch.arrow", :read) do |input|
+  Arrow::FileReader.open(input) do |reader|
     fields = reader.schema.fields
     reader.each_with_index do |record_batch, i|
       puts("=" * 40)
